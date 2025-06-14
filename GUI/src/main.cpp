4c4e4cc1f6961fd0069e36c65e7e2ef5d217da65
Harper Moreira <git@harper.rocks>
2025-06-13 21:12:25 -0300

    adicionar input do jogador

--- GUI/src/main.cpp
+++ GUI/src/main.cpp
@@ -25,7 +25,7 @@ struct SelectedCell {
 	std::string hint;
 };
 
-void game_cleanup(struct Game *game, int exit_status);
+void game_cleanup(struct Game *game, int exit_status, struct SelectedCell *selected_cell);
 bool load_media(struct Game *game, struct SelectedCell *selected_cell);
 bool sdl_initialize(struct Game *game);
 bool render_text(struct Game *game, std::string text);
@@ -33,7 +33,17 @@ void draw_canvas(struct Game *game, int canvas[8][8], struct SelectedCell *selec
 
 int main() {
 
-	char cells[8][8];
+	char cells[8][8] =
+	{
+		{'0', '0', '0', '0', '0', '0', '0', '0'},
+		{'0', '0', '0', '0', '0', '0', '0', '0'},
+		{'0', '0', '0', '0', '0', '0', '0', '0'},
+		{'0', '0', '0', '0', '0', '0', '0', '0'},
+		{'0', '0', '0', '0', '0', '0', '0', '0'},
+		{'0', '0', '0', '0', '0', '0', '0', '0'},
+		{'0', '0', '0', '0', '0', '0', '0', '0'},
+		{'0', '0', '0', '0', '0', '0', '0', '0'}
+	};
 	SelectedCell selected_cell = {
 		.hint = "Boas vindas às Cruzadinhas++!\nClique em uma célula, leia a dica e digite a palavra em seu teclado!"
 	};
@@ -64,11 +74,11 @@ int main() {
     };
 
     if (sdl_initialize(&game)) {
-        game_cleanup(&game, EXIT_FAILURE);
+        game_cleanup(&game, EXIT_FAILURE, &selected_cell);
     }
 
 	if (load_media(&game, &selected_cell)) {
-        game_cleanup(&game, EXIT_FAILURE);
+        game_cleanup(&game, EXIT_FAILURE, &selected_cell);
     }
 
     while (true) {
@@ -77,7 +87,7 @@ int main() {
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
             case SDL_QUIT:
-                game_cleanup(&game, EXIT_SUCCESS);
+                game_cleanup(&game, EXIT_SUCCESS, &selected_cell);
                 break;
             case SDL_MOUSEBUTTONDOWN:
 				drawing_text = true;
@@ -109,10 +119,121 @@ int main() {
 				selected_cell.i = mousepos_x;
 				selected_cell.j = mousepos_y;
 
-				// selected_cell.hint = cells[selected_cell.i][selected_cell.j].hint;
-            default:
+				// selected_cell.hint = cells[selected_cell.i][selected_cell.j].hint;]
+			default:
                 break;
             }
+			if(canvas[selected_cell.j][selected_cell.i] == 1)
+			{
+				const Uint8 *state = SDL_GetKeyboardState(NULL);
+				if (state[SDL_SCANCODE_A])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'a';
+				}
+				if (state[SDL_SCANCODE_B])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'b';
+				}
+				if (state[SDL_SCANCODE_C])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'c';
+				}
+				if (state[SDL_SCANCODE_D])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'd';
+				}
+				if (state[SDL_SCANCODE_E])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'e';
+				}
+				if (state[SDL_SCANCODE_F])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'f';
+				}
+				if (state[SDL_SCANCODE_G])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'g';
+				}
+				if (state[SDL_SCANCODE_H])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'h';
+				}
+				if (state[SDL_SCANCODE_I])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'i';
+				}
+				if (state[SDL_SCANCODE_J])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'j';
+				}
+				if (state[SDL_SCANCODE_K])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'k';
+				}
+				if (state[SDL_SCANCODE_L])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'l';
+				}
+				if (state[SDL_SCANCODE_M])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'm';
+				}
+				if (state[SDL_SCANCODE_N])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'n';
+				}
+				if (state[SDL_SCANCODE_O])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'o';
+				}
+				if (state[SDL_SCANCODE_P])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'p';
+					}
+				if (state[SDL_SCANCODE_Q])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'q';
+				}
+				if (state[SDL_SCANCODE_R])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'r';
+				}
+				if (state[SDL_SCANCODE_S])
+				{
+					cells[selected_cell.i][selected_cell.j] = 's';
+				}
+				if (state[SDL_SCANCODE_T])
+				{
+					cells[selected_cell.i][selected_cell.j] = 't';
+				}
+				if (state[SDL_SCANCODE_U])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'u';
+				}
+				if (state[SDL_SCANCODE_V])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'v';
+				}
+				if (state[SDL_SCANCODE_W])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'w';
+				}
+				if (state[SDL_SCANCODE_X])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'x';
+				}
+				if (state[SDL_SCANCODE_Y])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'y';
+				}
+				if (state[SDL_SCANCODE_Z])
+				{
+					cells[selected_cell.i][selected_cell.j] = 'z';
+				}
+			}
+
+			std::cout << "TECLA PRESSIONADA: " << cells[selected_cell.i][selected_cell.j] << std::endl;
+
         }
         SDL_RenderClear(game.renderer);
 
@@ -137,14 +258,22 @@ int main() {
         SDL_Delay(30);
     }
 
-    game_cleanup(&game, EXIT_SUCCESS);
+    game_cleanup(&game, EXIT_SUCCESS, &selected_cell);
 
     return 0;
 }
 
-void game_cleanup(struct Game *game, int exit_status) {
+void game_cleanup(struct Game *game, int exit_status, struct SelectedCell *selected_cell) {
 	SDL_DestroyTexture(game->cell);
-	SDL_DestroyTexture(game->cell_selected);
+	SDL_DestroyTexture(selected_cell->text);
+
+	for(int i = 0; i < 8; i++)
+	{
+		for(int j = 0; j < 0; j++)
+		{
+			SDL_DestroyTexture(game->char_image[i][j]);
+		}
+	}
 
 	SDL_DestroyTexture(game->text_image);
 	TTF_CloseFont(game->text_font);
@@ -228,6 +357,31 @@ bool render_text(struct Game *game, std::string text)
 	return false;
 }
 
+void render_number(struct Game *game, SDL_Rect *rect, char c, int i, int j)
+{
+	SDL_Rect trect;
+	trect.y = rect->x + 33;
+	trect.x = rect->y + 30;
+	trect.h = TEXT_SIZE + 10;
+	trect.w = TEXT_SIZE + 10;
+	const SDL_Rect crect = trect;
+
+	SDL_Surface*surface = TTF_RenderUTF8_Blended(game->text_font, &c, game->text_color);
+	if (!surface)
+	{
+		std::cout << "Error creating Surface: " << SDL_GetError() << std::endl;
+	}
+    game->text_rect.w = surface->w;
+    game->text_rect.h = surface->h;
+    game->char_image[i][j] = SDL_CreateTextureFromSurface(game->renderer, surface);
+    SDL_FreeSurface(surface);
+    if (!game->text_image)
+	{
+    	std::cout << "Error creating Texture: " << SDL_GetError() << std::endl;
+	}
+	SDL_RenderCopy(game->renderer, game->char_image[i][j], NULL, &crect);
+}
+
 void draw_canvas(struct Game *game, int canvas[8][8], struct SelectedCell *selected_cell, char cells[8][8])
 {
 	SDL_Rect rect;
@@ -240,40 +394,27 @@ void draw_canvas(struct Game *game, int canvas[8][8], struct SelectedCell *selec
 	{
 		for(int j = 0; j < 8; j++)
 		{
+
 			if (canvas[i][j] == 1)
 			{
 				SDL_RenderCopy(game->renderer, game->cell, NULL, &rect);
 			}
+
+			if (cells[i][j] != '0')
+			{
+				render_number(game, &rect, cells[i][j], i, j);
+			}
+
 			rect.x = rect.x + 100;
+
 		}
 		rect.x = 0;
 		rect.y = rect.y + 100;
 	}
-
-	if(canvas[selected_cell->j][selected_cell->i] != 0)
+	if (canvas[selected_cell->j][selected_cell->i] == 1)
 	{
 		rect.x = selected_cell->i * 100;
 		rect.y = selected_cell->j * 100;
 		SDL_RenderCopy(game->renderer, selected_cell->text, NULL, &rect);
-
-		rect.x += 27;
-		rect.y += 25;
-		rect.h = TEXT_SIZE * 2;
-		rect.w = TEXT_SIZE * 2;
-		const SDL_Rect crect = rect;
-		std::string c = "B";
-
-		SDL_Surface *surface = TTF_RenderUTF8_Blended(game->text_font, std::data(c), game->text_color);
-    	if (!surface) {
-        	std::cout << "Error creating Surface: " << SDL_GetError() << std::endl;
-    	}
-    	game->text_rect.w = surface->w;
-    	game->text_rect.h = surface->h;
-    	game->char_image[selected_cell->j][selected_cell->i] = SDL_CreateTextureFromSurface(game->renderer, surface);
-    	SDL_FreeSurface(surface);
-    	if (!game->text_image) {
-        	std::cout << "Error creating Texture: " << SDL_GetError() << std::endl;
-    	}
-		SDL_RenderCopy(game->renderer, game->char_image[selected_cell->j][selected_cell->i], NULL, &crect);
 	}
 }
